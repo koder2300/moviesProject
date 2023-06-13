72913a067202b58d8ee8d6295d55e7d0a56622f8
Dawid Kowalski <dawid@example.com>
2023-06-13 23:25:34 +0200

    not yet done next

--- renderInputItems.js
+++ renderInputItems.js
@@ -1,7 +1,32 @@
-const renderInputItems = () => {
-  const inputAdd = document.createElement("input");
-  //   inputAdd.setAttribute("type", "select");
-  //   console.log(inputAdd);
-  //   document.body.appendChild(inputAdd);
+const renderInputItems = (array, idItem) => {
+  const selectAdd = document.createElement("select");
+  selectAdd.setAttribute("id", idItem);
+  console.log(selectAdd);
+  document.body.appendChild(selectAdd);
+  //
+  let selectItem = document.getElementById("idItem");
+  let arrayAuthors = array;
+  console.log(arrayAuthors);
+  for (let i = 0; i < arrayAuthors.length; i++) {
+    let opt = arrayAuthors[i];
+    let el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectAdd.appendChild(el);
+  }
+  console.log(selectAdd);
 };
-renderInputItems();
+renderInputItems(getAvailableAuthors(movies), "selectAuthor");
+renderInputItems(getAvailableTitles(movies), "selectTitle");
+renderInputItems(getAvailableGenres(movies), "selectGenres");
+renderInputItems(getAvailableRatings(movies), "selectRating");
+//
+
+//
+document.querySelector("#selectAuthor").addEventListener("change", function () {
+  if (this.value == movies[1].author) {
+    console.log(`ok`);
+  } else {
+    console.log(`not`);
+  }
+});
