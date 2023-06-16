eef70c82618dbeb826da075323b614553b618a25
Dawid Kowalski <dawid@example.com>
2023-06-16 11:59:27 +0200

    not yet done 2

--- renderInput.js
+++ renderInput.js
@@ -1,4 +1,4 @@
-const renderInp = () => {
+const renderInpCreated = () => {
   const chooseInput = document.createElement("input");
   chooseInput.setAttribute("id", "idInput");
   chooseInput.setAttribute("placeholder", "Choose movie");
@@ -8,14 +8,37 @@ const renderInp = () => {
   console.log(val);
   document.getElementById("idInput").style.width = "30%";
 };
-renderInp();
-idInput.addEventListener("change", function () {
-  //   console.log(document.getElementById("idInput").value);
+renderInpCreated();
+/// INPUT PRÓBA 1
+//
+idInput.addEventListener("input", function () {
   let val = document.getElementById("idInput").value;
-  let arr = [];
-  arr.push(val);
-  movies.forEach((el) => {
-    console.log(el.author);
-  });
-  if (arr.includes()) console.log(arr);
+  //
+  let searchAut = "";
+  for (let i = 0; i < moviesArray.length; i++) {
+    if (val === moviesArray[i].author) {
+      let autMov = moviesArray[i].author;
+      let autGen = moviesArray[i].genres;
+      searchAut += `<div class="elAuthorDiv">Autor :${autMov} Gatunek :${autGen}</div>`;
+      document.querySelector("#container2").innerHTML = searchAut;
+    }
+  }
 });
+////////////////////
+// INPUT PRÓBA 2
+
+// idInput.addEventListener("input", function () {
+//   let val = document.getElementById("idInput").value;
+//   //
+//   for (let i = 0; i < moviesArray.length; i++) {
+//     let arr = moviesArray[i].author;
+//     // console.log(moviesArray[i].author);
+//     let found = val.split("").filter((char) => arr.includes(char));
+//     let foundLetter;
+//     if (found) {
+//       // console.log(found);
+//       foundLetter += `<div class="elAuthorDiv">Autor :${found}</div>`;
+//       document.querySelector("#container2").innerHTML = foundLetter;
+//     }
+//   }
+// });
--- renderItemsSelect.js
+++ renderItemsSelect.js
@@ -11,7 +11,7 @@ moviesArray.forEach((e) => {
     document.body.querySelector("#container2").appendChild(t);
 });
 //
-let changeTitles = (e) => {
+let changeAuthor = (e) => {
   e.target.value
     ? ((document.body.querySelector("#container2").innerHTML = ""),
       moviesArray.forEach((el) => {
@@ -30,6 +30,7 @@ let changeTitles = (e) => {
 
 //
 let changeGenres = (e) => {
+  console.log(`klik`);
   e.target.value
     ? ((document.body.querySelector("#container2").innerHTML = ""),
       moviesArray.forEach((el) => {
@@ -39,21 +40,18 @@ let changeGenres = (e) => {
             document.body.querySelector("#container2").appendChild(createdElm);
         }
       }))
-    : moviesArray.forEach((el) => {
+    : moviesArray((el) => {
         let createdElement = document.createElement("div");
         (createdElement.innerHTML = `autor : ${el.author}, gatunek :${el.genres}`),
-          document.body
-            .querySelector("#container2")
-            .appendChild(createdElement);
+          document.body.querySelector("#container").appendChild(createdElement);
       });
 };
 
-//
-// changeGenres();
+// // changeGenres();
 // document
 //   .querySelector("#selectGenres")
 //   .addEventListener("change", changeGenres);
 
 document
   .querySelector("#selectAuthor")
-  .addEventListener("change", changeTitles);
+  .addEventListener("change", changeAuthor);
