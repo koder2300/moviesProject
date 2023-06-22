18c85ee63b84b48c7286ced3a6540ed30860f224
Dawid Kowalski <dawid@example.com>
2023-06-22 23:58:24 +0200

    not yet done 9

--- renderSorted.js
+++ renderSorted.js
@@ -1,14 +1,13 @@
 const renderSort = (e) => {
   let arrSorted = [];
-  let arrayTitle = [];
-  let arraySortRating = [];
+
   //
   function sortN(a, b) {
     if (a < b) {
-      return -1;
+      return 1;
     }
     if (a > b) {
-      return 1;
+      return -1;
     }
   }
   //
@@ -16,35 +15,45 @@ const renderSort = (e) => {
     if (e.target.value === el) {
       console.log(`${el}`);
       if (el === "sortuj alfabetycznie A-Z") {
-        //       LIST
-        let divListTitle = document.querySelectorAll(".titleType");
-        //
-        divListTitle.forEach((el) => {
-          const titleContent = el.innerHTML;
-          arrayTitle.push(titleContent);
-          arrayTitle.sort(sortN);
-          return arrayTitle;
+        moviesArray.forEach((elm) => {
+          let arrAut = elm.author;
+          arrSorted.push(arrAut);
+          arrSorted.sort(function (a, b) {
+            return a.localeCompare(b);
+          });
+          return arrSorted;
         });
+        console.log(arrSorted);
       } else if (el === "sortuj alfabetycznie Z-A") {
         moviesArray.forEach((elm) => {
           let arrAut = elm.author;
           arrSorted.push(arrAut);
           arrSorted.sort(function (a, b) {
-            return b - a;
+            return b.localeCompare(a);
           });
           return arrSorted;
         });
+        console.log(arrSorted);
       }
       //
       else if (el === "sortuj wg oceny rosnąco") {
-        let arrayRating = document.querySelectorAll(".ratingType");
-        let arrFrom = Array.from(arrayRating);
-        arrFrom.forEach((el) => {
-          let arrRat = el.innerHTML;
-          arraySortRating.push(arrRat);
-          arraySortRating.sort(sortN);
-          return arraySortRating;
+        moviesArray.forEach((elm) => {
+          let arrAut = elm.rating;
+          arrSorted.push(arrAut);
+          // console.log(arrSorted);
+          arrSorted.sort();
+          return arrSorted;
         });
+        console.log(arrSorted);
+      } else if (el === "sortuj wg oceny malejąco") {
+        moviesArray.forEach((elm) => {
+          let arrAut = elm.rating;
+          arrSorted.push(arrAut);
+          // console.log(arrSorted);
+          arrSorted.sort(sortN);
+          return arrSorted;
+        });
+        console.log(arrSorted);
       }
       //
     }
